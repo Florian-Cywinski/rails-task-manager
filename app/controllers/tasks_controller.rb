c0079128e87afac92c87142b392f7a25cc3c8dcf
Florian Cywinski <florian.cywinski@protonmail.com>
2022-08-23 19:00:44 +0200

    Rails Task Manager v2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,24 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    # No need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    # No need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
